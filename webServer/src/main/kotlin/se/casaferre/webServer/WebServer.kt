f4567fa25d1f5efa49ecb4c83ef1f2c7550f0d80
Jörgen Andersson <jorgen@kollektiva.se>
2018-09-18 15:45:17 +0200

    Refactoring

--- webServer/src/main/kotlin/se/casaferre/webServer/WebServer.kt
+++ webServer/src/main/kotlin/se/casaferre/webServer/WebServer.kt
@@ -1,8 +1,7 @@
 package se.casaferre.webServer
 
-import se.casaferre.common.Temperature
 import se.casaferre.data.services.MonitorService
-import se.casaferre.webServer.controllers.FreemarkerController
+import se.casaferre.webServer.controllers.MidFreemarkerController
 import se.casaferre.webServer.controllers.MonitorController
 import se.casaferre.webServer.controllers.UtilsController
 import spark.Spark
@@ -30,46 +29,43 @@ class WebServer(port: Int, environment: String, filesLocation: String) {
             Spark.staticFiles.location(filesLocation)
         }
 
-        // response.status both get/set, will be 200 even if page not exists
         Spark.before("/*") { request: Request, response: Response ->
             request.session(true)
-            println("Before, session is new ${request.session().isNew()} and id is ${request.session().id()}")
-            println(request.pathInfo())
-        }
-
-        // response.status both get/set, will be 200 even if page not exists
-        Spark.after("/*") { request: Request, response: Response ->
-            println("After, status is ${response.status()}")
-            println(request.pathInfo())
         }
 
         // GZIP Everything
         Spark.after("/*") { _, response: Response -> response.header("Content-Encoding", "gzip") }
 
-        // response.status only get
-        Spark.afterAfter("/*") { request: Request, response: Response ->
-            println("AfterAfter, status is ${response.status()}")
-            println(request.pathInfo())
-            if (response.status() == 500) {
-                /* SEND EMAIL TO DEV */
-            }
-        }
-
 
         // Template server
-        FreemarkerController("/")
+        setupFreemarkerPages()
 
         // API server
-        // Test halt vill end up in Spark.after and Spark.afterAfter
-        Spark.get("/halt/:code") { request: Request, response: Response ->
-            val code = request.params(":code").toInt()
-            if (code != 200) {
-                // Spark.after is NOT called here but Spark.afterAfter is
-                Spark.halt(code, "This is a custom status code")
-            }
-            ""
+        setupRestPaths()
+
+        // Shortcuts
+        Spark.redirect.get("/mid99", "https://boiling-torch-802.firebaseapp.com/", Status.TEMPORARY_REDIRECT);
+
+
+        Spark.post("/set/:key/:value") { request: Request, response: Response ->
+            val key = request.params(":key")
+            val value = request.params(":value")
+            request.session().attribute(key, value)
+
+            println("In set")
+            println(request.session().attributes())
+            "success"
         }
 
+        // Init/Start server
+        Spark.init()
+    }
+
+    private fun setupFreemarkerPages() {
+        MidFreemarkerController("/")
+    }
+
+    private fun setupRestPaths() {
         // Controller for monitoring
         MonitorController(MonitorService())
 
@@ -78,12 +74,6 @@ class WebServer(port: Int, environment: String, filesLocation: String) {
             UtilsController()
         }
 
-        // Shortcuts
-        Spark.redirect.get("/mid99", "https://boiling-torch-802.firebaseapp.com/", Status.TEMPORARY_REDIRECT);
-
-
-        // Init/Start server
-        Spark.init()
     }
 
 }
